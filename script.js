5b84cb1a1d081f8c7070c244e4b772272b9c898d
EnemuoChidera45 <isrealdera@gmail.com>
2026-02-20 15:18:48 +0100

    Final changes to the etch-a-project

---
+++ script.js
@@ -0,0 +1,34 @@
+const container = document.querySelector("#container");
+const resetBtn = document.querySelector("#reset-btn");
+
+function createGrid(size) {
+  container.innerHTML = "";
+  const squareSize = 960 / size;
+  for (let i = 0; i < size * size; i++) {
+    const div = document.createElement("div");
+    div.classList.add("square");
+
+    div.style.width = `${squareSize}px`;
+    div.style.height = `${squareSize}px`;
+
+    div.addEventListener("mouseover", () => {
+      div.style.backgroundColor = "black";
+    });
+
+    container.appendChild(div);
+  }
+}
+
+resetBtn.addEventListener("click", () => {
+  let userSize = prompt("Enter squares(Max 100):");
+
+  userSize = parseInt(userSize);
+
+  if (isNaN(userSize) || userSize < 1 || userSize > 100) {
+    alert("Please enter a valid number between 1 and 100.");
+  } else {
+    createGrid(userSize);
+  }
+});
+
+createGrid(16);
